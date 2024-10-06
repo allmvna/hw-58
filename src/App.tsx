5122f7385172764f7d1d6bbb260d9cbc9ace7bab
Begimai Mederbekova <begimaialymbekova2@gmail.com>
2024-10-06 19:33:06 +0600

    Added a button config to the modal window.

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,42 @@ import "./App.css";
 import Modal from "./components/UI/Modal/Modal.tsx";
 import {useState} from "react";
 
-function App() {
-  const [showModal, setShowModal] = useState<boolean>(true);
 
-  const cancel = () => setShowModal(!showModal);
+function App() {
+  const [showModal, setShowModal] = useState<boolean>(false);
+
+  const cancel = () => setShowModal(false);
+
+  const arrayConfigButtons = [
+    {
+      type: "primary",
+      label: "Continue",
+      onClick: () => console.log("clicked continue"),
+    },
+    {
+      type: "danger",
+      label: "Close",
+        onClick: cancel,
+    },
+  ];
 
   return (
       <>
-        <Modal
-            show={showModal}
-            closeModal={cancel}
-            title="Some kinda modal title"
-        >
-          <p>This is modal content</p>
-        </Modal>
-      </>
-  )
+          <div className='text-center mt-5'>
+              <button onClick={() => setShowModal(true)}>Open modal-window</button>
+          </div>
 
+          <Modal
+              show={showModal}
+              closeModal={cancel}
+              title="Some kinda modal title"
+              arrayConfigButtons={arrayConfigButtons}
+          >
+              <p>This is modal content</p>
+          </Modal>
+
+      </>
+  );
 }
 
 export default App;
--- src/components/UI/Modal/Modal.tsx
+++ src/components/UI/Modal/Modal.tsx
@@ -1,34 +1,47 @@
-import React, {ReactNode} from 'react';
+import React from "react";
 
-interface ModalProps extends React.PropsWithChildren{
-    title: string;
-    show: boolean;
-    closeModal: () => void;
+interface ModalProps extends React.PropsWithChildren {
+  title: string;
+  show: boolean;
+  closeModal: () => void;
+
+  arrayConfigButtons: {
+    type: string;
+    label: string;
+    onClick: () => void;
+  }[];
 }
 
-const Modal: React.FC<ModalProps> = ({ show, children, closeModal, title}) => {
-    return (
-        <>
-            {show && (
-                <div
-                    className="modal-backdrop show"
-                    style={{display: 'block'}}
-                />
-            )}
-            <div className="modal show" style={{display: show ? "block" : "none"}}>
-            <div className="modal-dialog">
-                    <div className="modal-content">
-                        <div className="modal-header">
-                            <h1 className="modal-title fs-5">{title}</h1>
-                            <button className="btn-close" onClick={closeModal}>
-                            </button>
-                        </div>
-                        <div className="p-3">{children}</div>
-                    </div>
-                </div>
+const Modal: React.FC<ModalProps> = ({ show, children, closeModal, title, arrayConfigButtons }) => {
+  return (
+    <>
+      {show && (
+        <div className="modal-backdrop show" style={{ display: "block" }}  />
+      )}
+      <div className="modal show" style={{ display: show ? "block" : "none" }}>
+        <div className="modal-dialog">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h1 className="modal-title fs-5">{title}</h1>
+              <button className="btn-close" onClick={closeModal}></button>
             </div>
-        </>
-    );
+            <div className="p-3">{children}</div>
+            <div className="modal-footer">
+              {arrayConfigButtons.map((button, index) => (
+                  <button
+                      key={index}
+                      className={`btn btn-${button.type}`}
+                      onClick={button.onClick}
+                  >
+                    {button.label}
+                  </button>
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Modal;
